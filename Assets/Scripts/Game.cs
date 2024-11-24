8266c2f0edfaf57e8933e8885fe911583d88901e
PoemaPalermo <163904623+PoemaPalermo@users.noreply.github.com>
2024-11-24 06:38:23 -0300

    correccion en error de mivimiento y cambio de textos de scores a "No data yet"

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -18,19 +18,19 @@ public class Game : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) && direction!= Vector2.right)
         {
-            direction = Vector2.left;
+            direction = Vector2.left; 
         }
-        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) && direction != Vector2.left)
+        { 
             direction = Vector2.right;
         }
-        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) && direction != Vector2.down)
         {
             direction = Vector2.up;
         }
-        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) && direction != Vector2.up)
         {
             direction = Vector2.down;
         }
